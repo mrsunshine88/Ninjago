6fe858d82b4af71fd5a3dded16c97297e79d55b5
mrsunshine88 <apersson508@gmail.com>
2026-03-12 17:49:19 +0100

    Fixat säkerhetsspärr för Firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
+  // gitleaks:allow
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCjhJK0VuTyyJpiTCxQrqdJYL1KnPQ52J8",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "ninjago-elemental-clas.firebaseapp.com",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "ninjago-elemental-clas",
