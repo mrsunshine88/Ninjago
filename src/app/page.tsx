43a8bdb65dbb6760c4b0dc743e75061aed3fc2c1
mrsunshine88 <apersson508@gmail.com>
2026-03-13 22:23:15 +0100

    Complete fix for page.tsx and netlify build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,13 @@
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
-
 import nextDynamic from 'next/dynamic';
 
-
-
 const DynamicGame = nextDynamic(() => import('@/components/game/NinjagoGame'), {
-
   ssr: false,
-
   loading: () => <main className="min-h-screen bg-black" />
-
 });
 
-
-
 export default function Page() {
-
   return <DynamicGame />;
-
 }
